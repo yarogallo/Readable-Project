9a05d85f7751a3dc45f54a6547fd62971cd6fdd0
yanisleidi <yaroga88@yahoo.com>
2018-05-08 11:05:49 -0700

    Displayed the  author of each post on the home page and category view

--- src/components/presentational/ThumbnailPostList/index.js
+++ src/components/presentational/ThumbnailPostList/index.js
@@ -17,7 +17,10 @@ function ThumbnailPostList(props) {
 			<ul className="list-group list-group-flush">
 				{ posts.map( post => (
 					<li className="list-group-item post-thumbnail" key={post.id}>
-						<Link to={`/category/${post.category}/post/${post.id}`} ><h5>{post.title}</h5></Link>					
+						<p>
+							<Link to={`/category/${post.category}/post/${post.id}`} ><h5 className="d-inline">{post.title}</h5></Link>
+							<small className="text-muted text-capitalize">{` by: ${post.author}`}</small>
+						</p>					
 						<div>
 							<ScoreMenu scoreValue={post.voteScore} onVote={voteText => onVotePost(post.id, voteText)}/>
 							<DeleteButton onDelete={() => onDeletePost(post.id)} text="Want delete this post?"/>
